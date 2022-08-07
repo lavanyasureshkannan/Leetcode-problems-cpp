f40bd4ab2e2c446d804ab3b979b4e33bc3d4b9ec
Lavanya <79808091+lavanyasureshkannan@users.noreply.github.com>
2022-08-07 14:39:31 -0700

    Add files via upload

---
+++ tesla/1189. Maximum Number of Balloons.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+
+int maxNumberOfBalloons (string s)
+{
+    int count_b = 0;
+    int count_a = 0;
+    int count_l = 0;
+    int count_o = 0;
+    int count_n = 0;
+    // iterate through the array and count the frequency
+    for(int i=0; i<s.size(); i++)
+    {
+        if(s[i] == 'b') count_b++;
+        else if (s[i] == 'a') count_a++;
+        else if(s[i] == 'l') count_l++;
+        else if(s[i] == 'o') count_o++;
+        else if(s[i] == 'n') count_n++;
+    }
+    // finding the potential of each character
+    count_l = count_l/2;
+    count_o = count_o /2;
+    cout << min({count_b,count_a,count_o,count_l,count_n})<< endl;
+    return min({count_b,count_a,count_o,count_l,count_n});    
+}
+
+
+int main()
+{
+    string s = "loonbalxballpoon";
+    maxNumberOfBalloons (s);
+    return 0;
+
+}
+
+
+// TC:  O(N) We iterate over all the characters of string text which requires NN operations.
+// SC: O(1) All we need is the 55 variables to store the frequency of characters. Hence the space complexity is constant.
+
