0c0f1fcacae3fa8ee5a7a394b23778e393df7aad
Lavanya <79808091+lavanyasureshkannan@users.noreply.github.com>
2022-08-10 12:42:25 -0700

    Add files via upload

---
+++ oops/Polymorphism/function_overloading.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+/*
+    POLYMORPHISM -> simple means exists in more than one form. 
+    The entity behaves differently at different scenarios
+    
+    There are three ways you can achieve polymorphism
+    1. FUnction Overloading
+    2. Operator Overloading
+    3. Function Overriding
+    4. Virtual Functions 
+*/
+
+/*
+
+FUnction Overloading -> Two functions can have the same name if the number and/or type of arguments passed is different.
+
+*/
+
+
+int sum(int num1, int num2)
+{   
+    int total = num1 + num2;
+    cout << total << endl;
+    return total;
+}
+
+double sum(double num1, double num2)
+{
+    int total = num1+ num2;
+    cout << total << endl;
+    return total;
+}
+
+int main()
+{
+    sum(5,6); // calls the first function
+    sum(5.2,6.5); //calls the second function
+    return 0;
+}
