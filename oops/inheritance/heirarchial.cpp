ba8e0ef1efc2127801f790615cb8032513b2e4f5
Lavanya <79808091+lavanyasureshkannan@users.noreply.github.com>
2022-08-10 03:40:25 -0700

    Add files via upload

---
+++ oops/inheritance/heirarchial.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+
+/*
+ INHERITANCE
+ 1. MODES OF INHERITANCE -> public (can be accessed everywhere-> both inside and outside the class)
+                         -> protected (can be accessed in every subclass)
+                         -> private (cannot be accessed anywhere except parent class)
+
+ 2. TYPE OF INHERITANCE  -> Single Inheritance
+ `                       -> Multi-level Inheritance
+                         -> Multiple Inheritance
+                         -> Heirarachial Inheritance
+                         -> Hybrid Inheritance
+*/
+
+/*  HEIRARCHIAL INHERITANCE -> multiple sub classes derived from one base class
+
+    Base class          
+    /       \
+sub class1   sub class2          
+
+
+*/
+
+
+// Base class 1
+class Vehicle
+{
+    public:
+    void display()
+    {
+        cout << "This is a vehicle -> Display under base class" << endl;
+    }
+};
+
+
+
+// subclass 1
+class car : public Vehicle
+{
+    public:
+    void display_second()
+    {
+        cout << "This is a four wheeler -> Display under derived class" << endl;   
+    }
+};
+
+// second sub class
+class bus : public Vehicle
+{
+    public:
+    void display_sub()
+    {
+        cout << "This is a car -> display under second sub class" << endl;
+    }
+};
+
+
+int main()
+{
+    bus bus1;
+    bus1.display();
+    bus1.display_one();
+    bus1.display_sub();
+    return 0;
+}
