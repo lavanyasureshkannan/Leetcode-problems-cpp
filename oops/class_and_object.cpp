a3bd4d671db0183cc4ad86411b8fac098fe71af8
Lavanya <79808091+lavanyasureshkannan@users.noreply.github.com>
2022-08-09 22:46:40 -0700

    Add files via upload

---
+++ oops/class_and_object.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+
+//CLASS AND OBJECT
+
+// Class
+class group
+{
+    public:
+    string name;
+    int id;
+
+    void display()
+    {
+        cout << "The name of the student is: " << name << endl;
+        cout << "The id of the student is: "<< id << endl; 
+    }   
+};
+
+int main()
+{
+    // object
+    group G1;
+
+    // declare the variables inside G1
+    G1.name = "lavanya";
+    G1.id = 171;
+
+    // call the display function
+    G1.display();
+    
+    return 0; 
+}
+
