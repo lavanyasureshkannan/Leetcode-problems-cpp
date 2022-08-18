8284428cb5e7282bdd25490a14f440c8e4740aff
Lavanya <79808091+lavanyasureshkannan@users.noreply.github.com>
2022-08-18 00:47:48 -0700

    Add files via upload

---
+++ arrays/3Sum Closest.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+/*
+    TWO POINTER TECHNIQUE
+*/
+class Solution 
+{
+    public:
+    int threeSumClosest(vector<int>& nums, int target)
+    {
+        int result = nums[0] + nums[1] + nums[nums.size()-1];
+        sort(nums.begin(), nums.end());
+
+        // For the current position i, set lo to i + 1, and hi to the last index.
+        for(int i=0; i<nums.size()-2; i++)
+        {
+            int a_ptr = i+1;
+            int b_ptr = nums.size() -1;
+
+            while(a_ptr < b_ptr)
+            {
+                // Set sum to nums[i] + nums[low] + nums[high]
+                int curr_sum = nums[i] + nums[a_ptr] + nums[b_ptr];
+                if(curr_sum > target)
+                {
+                    b_ptr --;
+                }
+                else
+                {
+                    a_ptr ++;
+                }
+                if(abs(curr_sum - target) < abs(result-target))
+                {
+                    result = curr_sum;
+                }
+            }
+        }
+        return result;
+        }
+};
+
+
+
