195f032586abdc5dc6b20ffebd56260958123279
Lavanya <79808091+lavanyasureshkannan@users.noreply.github.com>
2022-08-23 12:30:54 -0700

    Add files via upload

---
+++ tesla/CLIMBING_STAIRS.cpp
@@ -0,0 +1,24 @@
+/*
+FIbonnoci number appproach
+*/
+
+class Solution 
+{
+public:
+    int climbStairs(int n) 
+    {
+        int a = 1;
+        int b = 1;
+        int c;
+        for(int i = 0; i < (n-1); i++){
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+        
+    }
+};
+
+// TC: O(N)
+// SC O (1)
