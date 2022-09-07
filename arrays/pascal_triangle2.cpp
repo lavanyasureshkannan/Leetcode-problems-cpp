0cc06ad27a48a451f5f9486207d30ed1c7a63364
Lavanya <79808091+lavanyasureshkannan@users.noreply.github.com>
2022-09-07 00:22:16 -0400

    Add files via upload

---
+++ arrays/pascal_triangle2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution
+{
+    public:
+    vector<int> getRow(int rowIndex)
+    {
+        vector<int> result(rowIndex+1, 1); // answer is going to be rowindex + 1
+        for(int i= 1; i<rowIndex; i++) // row
+        {
+            for(int j=i; j>0; j--) // col 
+            {
+                result[j] = result[j] + result[j-1]; 
+            }
+        }
+        return result;
+    }
+
+};
+
+// TC: O(K^2) -> calculating row and col
+// SC: O(K) -> No extra space
