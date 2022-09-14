0757fe4b41752d80cf198e6f66440ec95611afa1
Lavanya <79808091+lavanyasureshkannan@users.noreply.github.com>
2022-09-14 01:32:58 -0400

    Add files via upload

---
+++ STL_libraries/set_vs_unordered_set.cpp
@@ -0,0 +1,21 @@
+/*
+
+Difference between Set and Unordered Set
+-------------------------------------------------------------------------------------------------------------------------------------|
+    SET                                              |          UNORDERED SET                                                        |
+-----------------------------------------------------|-------------------------------------------------------------------------------|
+                                                     |                                                                               |
+1. Set stores elements in a sorted order	         |       Unordered Set stores elements in an unsorted order                      |
+-------------------------------------------------------------------------------------------------------------------------------------|
+2. Set stores/acquire unique elements only	         |       Unordered Set stores/acquire only unique values                         |
+-------------------------------------------------------------------------------------------------------------------------------------|
+3. Set uses Binary Search Trees for implementation	 |       Unordered Set uses Hash Tables for implementation                       |
+-------------------------------------------------------------------------------------------------------------------------------------|
+4. More than one element can be erased by giving     |                                                                               |
+the starting and ending iterator	                 |       We can erase that element for which the iterator position is given      |
+-------------------------------------------------------------------------------------------------------------------------------------|
+5. set<datatype> Set_Name;	                         |       unordered_set<datatype> UnorderedSet_Name;                              |
+--------------------------------------------------------------------------------------------------------------------------------------
+
+
+*/
