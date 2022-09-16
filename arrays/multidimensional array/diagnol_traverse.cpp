20238c383fbe90cca1d30335a2c1a646e12d76a9
Lavanya suresh Kannan <79808091+lavanyasureshkannan@users.noreply.github.com>
2022-09-16 18:03:35 -0400

    Add files via upload

---
+++ arrays/multidimensional array/diagnol_traverse.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+/*
+    no of diagnols => {{     0       } ->0
+                        {      2     } -> 1
+                        {      3     } -> 2
+                        {           } ->3
+                        {           }} ->4
+    i+j = 0+0 = 0
+            0+1 = 1 (put the element 01 in 1)
+            0+2 = 2......
+    after that reverse the vector which is in even digit
+*/
+
+
+
+class Solution 
+{
+    public:
+	vector<int> findDiagonalOrder(vector<vector<int>>& matrix) 
+    {
+        
+		int m = matrix.size();
+		int n = matrix[0].size();
+
+        
+		vector<vector<int>> diagnol(m+n-1); // find the diagnols
+		vector<int> v;
+
+        if(m == 0 || n == 0) return v;
+    
+		for(int i=0; i<m; i++)
+        {
+			for(int j=0; j<n; j++)
+            {
+				diagnol[i+j].push_back(matrix[i][j]); 
+			}
+		}
+		for(int i=0; i<diagnol.size(); i++)
+        {
+			if(i%2 == 0)
+			{
+				reverse(diagnol[i].begin(), diagnol[i].end());
+            }
+            for(int j=0; j<diagnol[i].size(); j++)
+			{
+            	v.push_back(diagnol[i][j]);
+		
+            }
+        }
+		return v;
+	}
+};
+
+
+//TC: O(N)
+// SC: O(1)
