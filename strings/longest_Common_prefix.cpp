673ba58925eb4f3d52346b833b7c28d3f0590bb3
Lavanya suresh Kannan <79808091+lavanyasureshkannan@users.noreply.github.com>
2022-09-16 21:23:56 -0400

    Add files via upload

---
+++ strings/longest_Common_prefix.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+class Solution 
+{
+public:
+    string longestCommonPrefix(vector<string>& strs) 
+    {
+        int len = strs.size();
+        
+        if(len == 0)
+            return "";
+        
+        sort(strs.begin(),strs.end());
+        string prefix;
+        string first = strs[0];
+        string last = strs[len -1];
+        
+        for(int i = 0; i < first.size(); i++)
+        {
+            if(first[i] ==last[i])
+            {
+                prefix = prefix + first[i];
+            }
+            else
+                break;
+            
+            
+        }
+        return prefix;
+        
+    }
+};
