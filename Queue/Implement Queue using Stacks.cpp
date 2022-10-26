5c80688f1f10e842ff99fd768658b76e1d8547d3
Lavanya suresh Kannan <79808091+lavanyasureshkannan@users.noreply.github.com>
2022-10-25 20:05:47 -0400

    Add files via upload

---
+++ Queue/Implement Queue using Stacks.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<numeric>
+#include<stack>
+using namespace std;
+
+class MyQueue 
+{
+    private:
+    stack<int> in_stk;
+    stack<int> out_stk;
+
+    public:
+    MyQueue()
+    {
+        
+    }
+    // Push element x to the back of queue...
+    void push(int x) 
+    {
+        in_stk.push(x);
+    }
+	// Remove the element from the front of the queue and returns it...
+    int pop() 
+    {
+        peek();
+        const int val = out_stk.top();
+        out_stk.pop();
+        return val;
+    }
+	// Get the front element...
+    int peek() 
+    {
+        if (out_stk.empty())
+        while (!in_stk.empty())
+            out_stk.push(in_stk.top()), in_stk.pop();
+        return out_stk.top();
+    }
+	// Return whether the queue is empty...
+    bool empty() 
+    {
+        return in_stk.empty() && out_stk.empty();
+    }
+
+};
