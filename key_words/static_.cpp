029df6e013a7dec30944484fea953365e549cd54
Lavanya suresh Kannan <79808091+lavanyasureshkannan@users.noreply.github.com>
2022-11-08 04:28:56 -0500

    Update static_.cpp

--- key_words/static_.cpp
+++ key_words/static_.cpp
@@ -1,41 +1,53 @@
 #include<iostream>
 using namespace std;
 
-// When a variable is declared as static, space for it gets allocated for the lifetime of the program.
-// accessing STATIC VARIABLE and STATIC FUNCTIONS INSIDE THE CLASS.
+/*
+    STATIC VARIABLE
+    1. When you declare a variable as static then the memory for that variable is allocated throughout the lifetime of the program.
+    2. Static variable is common for all the objects you create from that particular class
+    3. Static functions inside the class can access only static varibles.
+    4. Non-static member functions can access both.
+    5. The static variables or functions declared outside the class or struct will have a scope for that particular translation unit and cannot be accessed everywhere unlike the global variable.
+*/
+
 
 class base
 {
     public:
-    base() {}
-    static int num1;
-    static int num2;
+    
+    int x;
+    static int y;
 
-    int i;
-    static int return_num(int i)
+    static void display()
     {
-        return i;
+        cout << y << endl;
     }
-    static int add(int num1, int num2)
-    {
-        return num1 + num2;
-    }
+
 };
 
-// datatype classname :: static variable
-int base :: num1 = 3;
-int base :: num2 = 4;  
+// create a memory for the static variable
+// this y will be common for all the b1,b2,b3 and so on 
+int base :: y = 60;
 
-// You cannot access the non-static member variable outside the class (like below). 
-// int base :: i = 10;
 
+static int add(int a, int b)
+{
+    return a+b;
+}
 
 int main()
 {
     base b1;
-    cout << b1.num1 << " " << b1.num2 << endl;
-    cout << b1.add(b1.num1, b1.num2) << endl;
+    b1.x = 10;
+    base::display();
 
-    cout << base::add(6,7) << endl;
+
+    base b2;
+    b2.x = 20;
+
+    //cout << b1.x << " " << b2.x << endl; 
+    //cout << b1.y << " " << b2.y << endl;
+
+    
     return 0;
 }
