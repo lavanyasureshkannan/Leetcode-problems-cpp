8e05164a758e413aac5bfba2b59bc07dcc1f7be1
Lavanya suresh Kannan <79808091+lavanyasureshkannan@users.noreply.github.com>
2022-11-13 01:26:49 -0500

    Add files via upload

---
+++ key_words/auto.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+/* Auto keyword:
+    it is used for type deduction. based on the initialization the compiler understands the data type 
+*/
+
+int main()
+{
+    auto a = 10;
+    auto c = 'd';
+}
