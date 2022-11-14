82a4c6728be37f83e158ab6546da81a9c8c591dc
Lavanya suresh Kannan <79808091+lavanyasureshkannan@users.noreply.github.com>
2022-11-13 20:23:28 -0500

    Add files via upload

---
+++ key_words/threading/p_thread.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+/*
+    In a Unix/Linux operating system, the C/C++ languages provide the POSIX thread(pthread) standard API(Application program Interface) for all thread related functions.
+    It allows us to create multiple threads for concurrent process flow. 
+*/
