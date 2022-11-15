a92bed9c6906eed7bf3724031d4c07ac0f0654f9
Lavanya suresh Kannan <79808091+lavanyasureshkannan@users.noreply.github.com>
2022-11-15 02:14:49 -0500

    Update and rename 2.First Bad Version to 2.First Bad Version.cpp

--- sorting and searching/2.First Bad Version.cpp
+++ sorting and searching/2.First Bad Version.cpp
@@ -18,8 +18,14 @@ public:
         while(left < right)
         {
             int mid = left+(right-left)/2;
-            if(isBadVersion(mid)) right = mid;
-            else left = mid + 1;
+            if(isBadVersion(mid)) 
+            {
+                right = mid;
+            }
+            else
+            {
+            left = mid + 1;
+            }
         }
         return left;
             
