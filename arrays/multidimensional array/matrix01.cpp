56bf4ac6e6b6d3ea4fd4e0ef01543632b342215d
Lavanya suresh Kannan <79808091+lavanyasureshkannan@users.noreply.github.com>
2022-11-14 21:11:55 -0500

    Update matrix01.cpp

--- arrays/multidimensional array/matrix01.cpp
+++ arrays/multidimensional array/matrix01.cpp
@@ -17,38 +17,51 @@ Pop the cell from queue, and examine its neighbors. If the new calculated distan
 */
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
-        int rows = matrix.size();
-        if (rows == 0)
-            return matrix;
-        int cols = matrix[0].size();
-        vector<vector<int>> dist(rows, vector<int> (cols, INT_MAX));
-        queue<pair<int, int>> q;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (matrix[i][j] == 0) {
-                    dist[i][j] = 0;
-                    q.push({ i, j }); //Put all 0s in the queue.
-                }
-            }
-        }
-
-        int dir[4][2] = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
-        while (!q.empty()) {
-            pair<int, int> curr = q.front();
-            q.pop();
-            for (int i = 0; i < 4; i++) {
-                int new_r = curr.first + dir[i][0], new_c = curr.second + dir[i][1];
-                if (new_r >= 0 && new_c >= 0 && new_r < rows && new_c < cols) {
-                    if (dist[new_r][new_c] > dist[curr.first][curr.second] + 1) {
-                        dist[new_r][new_c] = dist[curr.first][curr.second] + 1;
-                        q.push({ new_r, new_c });
-                    }
-                }
-            }
-        }
-        return dist;
+    vector<vector<int>> updateMatrix(vector<vector<int>>& matrix)
+{
+    int row = matrix.size();
+    if(row == 0)
+    {
+        return matrix;
     }
+    int col = matrix[0].size();
+    vector<vector<int>> distance(row, vector<int>(col, INT_MAX));
+    queue<pair<int,int>>q;
+    vector<vector<int>> direction = {{1,0},{-1,0},{0,1},{0,-1}};
+    for(int i =0; i<row; i++)
+    {
+        for(int j=0; j<col; j++)
+        {
+            if(matrix[i][j] == 0)
+            {
+                distance[i][j] = 0;
+                q.push({i,j});
+            }
+        }
+    }
+    while(!q.empty())
+    {
+        pair<int,int> curr = q.front();
+        q.pop();
+        for(int i = 0; i<direction.size(); i++)
+        {   
+            int new_row = curr.first + direction[i][0]; 
+            int new_col = curr.second + direction[i][1];
+            if(new_row >= 0 && new_row < row && new_col >= 0 && new_col < col)
+            {
+                if(distance[new_row][new_col] > distance[curr.first][curr.second] + 1)
+                {
+                    distance[new_row][new_col] = distance[curr.first][curr.second] + 1;
+                    q.push({new_row, new_col});
+                }
+             }
+        }
+    }
+    return distance;
+}
+
+// TC: O(r,c)
+// SC: O(r,c)
 };
 
 // TC: O(R.C)
