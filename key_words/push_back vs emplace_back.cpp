91177cadea012cc528e9004cdbf0f43cc580d450
Lavanya suresh Kannan <79808091+lavanyasureshkannan@users.noreply.github.com>
2022-12-13 23:30:37 -0500

    Add files via upload

---
+++ key_words/push_back vs emplace_back.cpp
@@ -0,0 +1,11 @@
+/*
+Push_back: 
+1. Push_back adds a new element at the end. 
+2. It first creates a temporary object by calling a constructor.
+
+Emplace_back: 
+1. It also Inserts a new element at the end. 
+2. It does not create a temporary object. The object is directly created in the vector. 
+3. Due to this, the efficiency is increased.
+
+*/
