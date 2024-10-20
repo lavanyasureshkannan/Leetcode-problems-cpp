4a4756a58fc8625ad6f0c2dabf5805e130fe86a9
Lavanya suresh Kannan <79808091+lavanyasureshkannan@users.noreply.github.com>
2024-10-20 01:55:40 -0700

    Update Plus One.cpp

--- arrays/Plus One.cpp
+++ arrays/Plus One.cpp
@@ -8,26 +8,37 @@ using namespace std;
 // case 2 - 1,2,9 --> change 9 to 0
 // case 3 - 9,9,9 --> change 9 to 0 and index goes out of range so add 1 to the beginning.
 
-class Solution 
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+vector<int> plus_one(vector<int>&nums)
 {
-public:
-    vector<int> plusOne(vector<int>& digits) 
+    int n = nums.size();
+    for(int i=n-1; i>=0; i--)
     {
-        int index = digits.size() - 1;
-        while (index >= 0) 
+        if(nums[i] < 9)
         {
-            if (digits[index] == 9)
-            {
-                digits[index] = 0;
-            }
-            else
-            {
-                digits[index]+=1;
-                return digits;
-            }
-            index --;
+            nums[i]++;
+            return nums;
         }
-        digits.insert(digits.begin(),1);
-        return digits;
+        nums[i] = 0;
     }
-};
+    nums.insert(nums.begin(), 1);
+    return nums;
+}
+
+
+int main() {
+    vector<int> digits = {4, 3, 2, 9}; // Represents 4329
+    vector<int> result = plus_one(digits);
+
+    cout << "Result: ";
+    for (int digit : result) {
+        cout << digit;
+    }
+    cout << endl; // Outputs: Result: 4330
+
+    return 0;
+}
