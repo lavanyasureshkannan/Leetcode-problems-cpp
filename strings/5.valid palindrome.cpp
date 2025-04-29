ef2bd26d981772a5ea9d4c52fe407e57adbf8b13
Lavanya suresh Kannan <79808091+lavanyasureshkannan@users.noreply.github.com>
2025-04-28 23:04:47 -0700

    Update 5.valid palindrome.cpp

--- strings/5.valid palindrome.cpp
+++ strings/5.valid palindrome.cpp
@@ -2,33 +2,33 @@
 
 class Solution {
 public:
-    bool isPalindrome(string s) {
-        int size = s.length();
-        // create a string
-        string st;
-        // check for the valid characters
-        for (int i=0;i<size;i++){
-            if(s[i]>='A'&& s[i]<='Z'){
-                st+=tolower(s[i]); // tolower is to convert into lower case
-            }
-            if(s[i]>='a' && s[i]<='z'){
-                st+=s[i];
-            }
-            if(s[i]>='0' && s[i] <='9'){
-                st+=s[i];
-            }
+bool valid_paranthesis(string s)
+{
+    string st = " ";
+    for(char c : s)
+    {
+        if(isalnum(c))
+        {
+            st += tolower(c);
         }
-        // use two pointer technique
-        int left =0;
-        int right = st.length() - 1;
-        while(left<right){
-            if(st[left]!=st[right]){
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
     }
+
+    int left = 0;
+    int right = st.size() - 1;
+    while(left < right)
+    {
+        if(st[left] != st[right])
+        {
+            return false;
+        }
+        
+        left++;
+        right--;
+        
+    }
+    return true;
+}
+
+
 };
         
